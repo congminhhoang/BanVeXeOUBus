8522f3b380e18df4a601d30305bffae209f16cf3
Le Anh Quan <leanhquansinhk10@gmail.com>
2022-01-12 22:48:51 +0700

    update 1/12 Quan

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.mycompany.quanlyvexe {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.base;
+
+    opens com.mycompany.quanlyvexe to javafx.fxml;
+    exports com.mycompany.quanlyvexe;
+}
