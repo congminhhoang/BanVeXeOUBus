3a12acb4da04f31bbdb4496392a5776cd201d036
Luong <Luonghung2782001@gmail.com>
2022-01-15 17:22:06 +0700

    update GD code Chuyen di-Luong 15/1

--- src/main/java/com/hcm/conf/jdbcUtils.java
+++ src/main/java/com/hcm/conf/jdbcUtils.java
@@ -4,9 +4,11 @@
  */
 package com.hcm.conf;
 
+import com.qlbv.pojo.Xe;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javafx.collections.ObservableList;
 
 
 
@@ -30,4 +32,9 @@ public class jdbcUtils {
     public static Connection getConn() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://localhost/quanlyvexe","root","123456789");
     }
+
+    public static ObservableList<Xe> getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
