76f8dad4a3ac01e5325c0e834801a54f630bd30c
Minh Dao <tmd.160301@gmail.com>
2022-01-16 10:32:01 +0700

    switch

--- src/main/java/com/mycompany/quanlyvexe/FXMLLoginController.java
+++ src/main/java/com/mycompany/quanlyvexe/FXMLLoginController.java
@@ -4,9 +4,14 @@
  */
 package com.mycompany.quanlyvexe;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +27,20 @@ public class FXMLLoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+    public void switchAdmin(ActionEvent event) throws IOException {
+             FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("FXMLChuyenDi.fxml"));     
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("ADMIN");
+            stage.show();
+    }
+    public void switchNV(ActionEvent event) throws IOException {
+             FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("FXMLVeXe.fxml"));     
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("NHANVIEN");
+            stage.show();
+    }
 }
